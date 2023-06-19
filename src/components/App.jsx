5a94535b69d96a90c061351ab71edef9aefb74bb
Soundlover1984 <denys.kovtun@gmail.com>
2023-06-19 18:32:24 +0300

    added components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
 import user from 'data/user.json'
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
+import css from './App.module.css';
+
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+
+
 
 
 export const App = () => {
   return (
-    <Profile
+<div className={css.container}>
+<Profile
   username={user.username}
   tag={user.tag}
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
 />
+<Statistics title="Upload stats" stats={data} />
+<FriendList friends={friends} />
+</div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,31 @@
+
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import { FriendListItem } from '../FriendListItem/FriendListItem';
+
+
+export const FriendList= ({ friends }) => {
+    return (
+        <ul className={css.friendList}>
+        {friends.map(({ id, isOnline, avatar, name }) => (
+          <FriendListItem
+            key={id}
+            isOnline={isOnline}
+            avatar={avatar}
+            name={name}
+          />
+        ))}
+      </ul>
+    );
+}
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        avatar: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        isOnline: PropTypes.bool.isRequired,
+      })
+    ).isRequired,
+  };
