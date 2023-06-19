21ace4420afb4596d53eb9ae5d9b84330ad7f777
Soundlover1984 <denys.kovtun@gmail.com>
2023-06-19 21:04:11 +0300

    added styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,8 +7,7 @@ import css from './App.module.css';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
-
-
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 
 export const App = () => {
@@ -23,6 +22,7 @@ export const App = () => {
 />
 <Statistics title="Upload stats" stats={data} />
 <FriendList friends={friends} />
+<TransactionHistory items={transactions} />
 </div>
   );
 };
